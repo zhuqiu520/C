4fa381f32992c1f69aee7f16e12f1fb347765acc
zhuqiu520 <zhuqiu945@gmail.com>
2024-07-22 22:51:45 +0800

    最新笔记

--- power/power/FileName.c
+++ power/power/FileName.c
@@ -2,47 +2,58 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{
-	printf("愿世界和平，刚好遇见你\n");//printf打印字
-	printf("csgo\n");//同上
-	printf("%d\n", 886);//%d表示整形函数(整数),打印数字，也可写数字打印出字符
-	printf("%d\n",'b');//同上
-	printf("%c\n", 99);//%c表示字符函数，数字打出字符,可直接打印字符
-	printf("%c\n", 'u');//同上
-	printf("%f\n", 3.149527);//%f打印小数，浮点型
-	printf("%s\n", "app");//%s打印字符串
-	//int i =0;//尝试打印多个字符(循环)   “字符是单个，用单引号，字符串是多个，用双引号”
+//int main()
+//{
+//	printf("愿世界和平，刚好遇见你\n");//printf打印字
+//	printf("csgo\n");//同上
+//	printf("%d\n", 886);//%d表示整形函数(整数),打印数字，也可写数字打印出字符
+//	printf("%d\n",'b');//同上
+//	printf("%c\n", 99);//%c表示字符函数，数字打出字符,可直接打印字符
+//	printf("%c\n", 'u');//同上
+//	printf("%f\n", 3.149527);//%f打印小数，浮点型
+//	printf("%s\n", "app");//%s打印字符串
+//	//int i =0;//尝试打印多个字符(循环)   “字符是单个，用单引号，字符串是多个，用双引号”
+//
+//	//for (i = 31; i < 121; i++)
+//	//{
+//	//	printf("%c ", i);
+//	//}
+//	int a = 88;
+//	printf("%zu\n", sizeof(int));
+//	printf("%zu\n", sizeof(a));//用来算字符数量
+//	printf("%c\n", '\'');//"\'"用来打印单引号
+//	printf("%s\n", "\"");//"\"“打印字符串内的双引号
+//	printf("help:\\test\\tset.com\n");//"\\"防止被转义字符\转义字符
+//	printf("%c\n", 'x',"\a");//"\a"用来发出警报
+//	printf("asdd\bfgh\n");//"\b"用来回退字符，但是并不删除
+//	printf("愿世界被温柔以待\n");//"\n"用来换行
+//	printf("abcdefg\r");//"\r"回车，用于让光标回到本行的开头
+//	printf("abc\tefg\thil\tadwdadwda\tdajdwa\n");//"\t"用于对齐八个宽度的字符，也可以是四个，根据字符长度来对齐，超过八个,前八个不会生效
+//	printf("%c\n", '456');//"\ddd"表示一到三个八进制数字，表示字符，八进制数字里面不能有八
+//	printf("%c\n", '\x56');//"\xdd"表示两个十六进制数字‘打印字符
+//	printf("%s\n", "java\0");//"\0"表示字符串的结束
+//	/*这是一个注释*/
+////	int a = 10;// 创建了一个整型的变量叫a，并赋值为10
+//	printf("%zd\n", sizeof(char));//sizeof用来计算字符长度
+//	printf("%zd\n", sizeof(_Bool));
+//		printf("%zd\n", sizeof(short));
+//		printf("%zd\n", sizeof(int));
+//		printf("%zd\n", sizeof(long));
+//		printf("%zd\n", sizeof(long long));
+//		printf("%zd\n", sizeof(float));
+//		printf("%zd\n", sizeof(double));
+//		printf("%zd\n", sizeof(long double));
+//		printf("char 类型的大小是: %zu 字节\n", sizeof(char));//更简单明了
+//
+//	   return 0;
+//}
+int main() {
+    int a;
+    while (scanf("%d", &a) != EOF)
+        if (a = 140) { // 注意 while 处理多个 case
+            // 64 位输出请用 printf("%lld") to 
 
-	//for (i = 31; i < 121; i++)
-	//{
-	//	printf("%c ", i);
-	//}
-	int a = 88;
-	printf("%zu\n", sizeof(int));
-	printf("%zu\n", sizeof(a));//用来算字符数量
-	printf("%c\n", '\'');//"\'"用来打印单引号
-	printf("%s\n", "\"");//"\"“打印字符串内的双引号
-	printf("help:\\test\\tset.com\n");//"\\"防止被转义字符\转义字符
-	printf("%c\n", 'x',"\a");//"\a"用来发出警报
-	printf("asdd\bfgh\n");//"\b"用来回退字符，但是并不删除
-	printf("愿世界被温柔以待\n");//"\n"用来换行
-	printf("abcdefg\r");//"\r"回车，用于让光标回到本行的开头
-	printf("abc\tefg\thil\tadwdadwda\tdajdwa\n");//"\t"用于对齐八个宽度的字符，也可以是四个，根据字符长度来对齐，超过八个,前八个不会生效
-	printf("%c\n", '456');//"\ddd"表示一到三个八进制数字，表示字符，八进制数字里面不能有八
-	printf("%c\n", '\x56');//"\xdd"表示两个十六进制数字‘打印字符
-	printf("%s\n", "java\0");//"\0"表示字符串的结束
-	/*这是一个注释*/
-//	int a = 10;// 创建了一个整型的变量叫a，并赋值为10
-	printf("%zd\n", sizeof(char));//sizeof用来计算字符长度
-	printf("%zd\n", sizeof(_Bool));
-		printf("%zd\n", sizeof(short));
-		printf("%zd\n", sizeof(int));
-		printf("%zd\n", sizeof(long));
-		printf("%zd\n", sizeof(long long));
-		printf("%zd\n", sizeof(float));
-		printf("%zd\n", sizeof(double));
-		printf("%zd\n", sizeof(long double));
-		printf("char 类型的大小是: %zu 字节\n", sizeof(char));//更简单明了
-	   return 0;
+            printf("Genius");
+        }
+    return 0;
 }
--- zuoye/Project1/Project1/FileName.cpp
+++ zuoye/Project1/Project1/FileName.cpp
@@ -2,133 +2,199 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-你是天才!!!!!
-int main() {
-    int a;
-    while (scanf("%d", &a) != EOF)
-        if (a >= 140) { // 注意 while 处理多个 case
-            // 64 位输出请用 printf("%lld") to 
-            printf("Genius\n");
-        }
-    return 0;
-}
-比大小!!!!!
-int main() {
-    int a, b;
-    while (scanf("%d %d", &a, &b) == 2) {
-        if (a > b) {
-            printf("%d>%d\n", a, b);
-        }
-        else if (a < b) {
-            printf("%d<%d\n", a, b);
-        }
-        else {
-            printf("%d=%d\n", a, b);
-        }
-    }
-
-整数除以5!!!!
-int main() {
-    int M;
-    scanf("%d", &M);
-    if (M % 5 == 0) {
-        printf("YES\n");
-    }
-    else {
-        printf("NO\n");
-    }
-int main()
-{
-	int a;
-		scanf("%d", &a);
-		if (a >= 140) {
-			printf("Genius");
-		}
-计算商和余数!!!!!
-int main() {
-    int a, b, c, d;
-    scanf("%d %d", &a, &b);
-    c = a / b;
-    d = a % b;
-    printf("%d %d", c, d);
-
-你发财了测试!!!!!
-int main()
-{
-	int a;
-	printf("请输入一个数字\n");
-	while (scanf("%d", &a))
-	{
-		if (a >=500) {
-			printf("你发财啦\n");
-		}
-	}  
-		
-		
-			
-
-      return 0;
-	
-}
-除法测试!!!!!
-int main()
-{
-	int M;
-	printf("请输入数字\n");
-	while (scanf("%d", &M))
-	{
-		if (M % 26 == 0)
-			printf("YES\n");
-		else
-			printf("NO\n");
-
-
-	}  
-	
-
-	
-	
-	return 0;
-}
-除法运算!!!!!!!
-int main()
-{
-	int a, b, c, d;
-	printf("输入数字得出结果\n");
-	while (scanf("%d %d", &a, &b))
-	{
-		if (b == 0)
-			printf("错误，除数不能为0\n");
-		continue;
-	}
-	c = a / b;
-	d = a % b;
-	printf("%d %d", c, d);
-	return 0;
-}
-智商检测器!!!!!
-int main()
-{
-	char name[100];
-	int a;
-	srand((unsigned int)time(NULL));
-
- 	printf("请输入你的名字:\n");
-	scanf("%99s", name);
-	a = rand() % 240 + 1;
-	printf("你的智商是: %d\n", a);
-		if (a >180 ) {
-			printf("你的智商爆表啦！！\n");
-		}
-		else if (a >= 140) {
-			printf("你是天才！！！\n");
-		}
-		else if (0<a < 80){
-			printf("很遗憾，你是一个弱智\n");
-		}
-	
-	
-
-	return 0;
-}
-
+//你是天才!!!!!
+//int main() {
+//    int a;
+//    while (scanf("%d", &a) != EOF)
+//        if (a >= 140) { // 注意 while 处理多个 case
+//            // 64 位输出请用 printf("%lld") to 
+//            printf("Genius\n");
+//        }
+//    return 0;
+//}
+//比大小!!!!!
+//int main() {
+//    int a, b;
+//    while (scanf("%d %d", &a, &b) != EOF) {
+//        if (a > b) 
+//            printf("%d>%d\n", a, b);
+//        
+//        else if (a < b) 
+//            printf("%d<%d\n", a, b);
+//        
+//        else 
+//            printf("%d=%d\n", a, b);
+//        }
+//           return 0;
+//    }
+//////
+//整数除以5!!!!
+//int main() {
+//    int M;
+//    scanf("%d", &M);
+//    if (M % 5 == 0) {
+//        printf("YES\n");
+//    }
+//    else {
+//        printf("NO\n");
+//    }
+//int main()
+//{
+//	int a;
+//		scanf("%d", &a);
+//		if (a >= 140) {
+//			printf("Genius");
+//		}
+//计算商和余数!!!!!
+//int main() {
+//    int a, b, c, d;
+//    scanf("%d %d", &a, &b);
+//    c = a / b;
+//    d = a % b;
+//    printf("%d %d", c, d);
+//
+//你发财了测试!!!!!
+//int main()
+//{
+//	int a;
+//	printf("请输入一个数字\n");
+//	while (scanf("%d", &a))
+//	{
+//		if (a >=500) {
+//			printf("你发财啦\n");
+//		}
+//	}  
+//		
+//		
+//			
+//
+//      return 0;
+//	
+//}
+//整除测试!!!!!
+//int main()
+//{
+//	int M;
+//	printf("请输入数字\n");
+//	while (scanf("%d", &M))
+//	{
+//		if (M % 26 == 0)
+//			printf("YES\n");
+//		else
+//			printf("NO\n");
+//
+//
+//	}  
+//	
+//
+//	
+//	
+//	return 0;
+//}
+//除法运算!!!!!!!
+//int main()
+//{
+//	int a, b, c, d;
+//	printf("输入数字得出结果\n");
+//	while (scanf("%d %d", &a, &b))
+//	{
+//		if (b == 0)
+//			printf("错误，除数不能为0\n");
+//		continue;
+//	}
+//	c = a / b;
+//	d = a % b;
+//	printf("%d %d", c, d);
+//	return 0;
+//}
+//智商检测器!!!!!
+//int main()
+//{
+//	char name[100];
+//	int a;
+//	srand((unsigned int)time(NULL));
+//	printf("《智商检测仪器》\n");
+// 	printf("请输入你的名字:\n");
+//	scanf("%99s", name);
+//	a = rand() % 220 + 1;
+//	printf("你的智商是: %d\n", a);
+//		if (a >180 ) {
+//			printf("你的智商爆表啦！！！\n");
+//		}
+//		else if (a<=180 &&a>= 140) {
+//			printf("你就是天才！！\n");
+//		}
+//		else if (a>=80 && a<140 ) {
+//			printf("你拥有正常智商！\n");
+//		}		
+//		else if (a>0 && a<80){
+//			printf("很遗憾，你是一个弱智\n");
+//		}
+//	
+//	
+//
+//	return 0;
+//}
+//// 打印循环符号
+//int main()
+//{
+//    int a;
+//    while (scanf("%d", &a) !=EOF)
+//    {
+//        int i ;
+//        for (i = 0; i < a; i++)
+//        {
+//            printf("*");
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+//打印一百之中三的倍数
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 100; i++) {
+//		if (i % 3 == 0) {
+//			printf("%d ", i);
+//		}
+//
+//
+//}
+//		
+//
+//	return 0;
+//}
+//int main() {
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	printf("请输入三个数字，用空格分隔：\n");
+//	while (scanf("%d %d %d", &a, &b, &c) != EOF) {
+//		if (a >= b && a >= c) {
+//			if (b >= c) {
+//				printf("%d %d %d \n", a, b, c);
+//			}
+//			else {
+//				printf("%d %d %d \n", a, c, b);
+//			}
+//		}
+//		else if (b >= a && b >= c) {
+//			if (a >= c) {
+//				printf("%d %d %d \n", b, a, c);
+//			}
+//			else {
+//				printf("%d %d %d \n", b, c, a);
+//			}
+//		}
+//		else {
+//			if (a >= b) {
+//				printf("%d %d %d \n", c, a, b);
+//			}
+//			else {
+//				printf("%d %d %d \n", c, b, a);
+//			}
+//		}
+//	}
+//	return 0;
+//}
